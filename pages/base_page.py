9b33dc6fa0439e71aa97744425c189de268f8f6c
Elena Khaurova <elena.haurova@gmail.com>
2019-11-25 16:29:46 +0300

    удалена одна закомментированная строчка

--- pages/base_page.py
+++ pages/base_page.py
@@ -56,7 +56,6 @@ class BasePage:
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
-        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
